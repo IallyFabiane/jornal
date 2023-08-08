79eb0b349d7ec82d3019170e8cab098f9019ded7
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-08 11:55:02 -0300

    crinado classe simples

--- tmp/teste.php
+++ tmp/teste.php
@@ -4,10 +4,21 @@
             echo "hello world, my name is ";
             $nome = " Ially! ";
             print "$nome<br>";
+
             $boll = true;
             print "$boll<br>";
+
             $group = array("hello", "world");
             print_r($group);
+
+            class Noticia {
+                function imprimir() {
+                    return "<br><strong>PHP é multifuncional.</strong>";
+                }
+            }
+
+            $noticia = new Noticia;
+            print($noticia->imprimir()); /*-> significa a notação de ponto em PHP */
         ?>
     </body>
 </html>
