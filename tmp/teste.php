80b457dfd6b02c47fec63e4e559e2d35a7fd5428
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-08 11:48:20 -0300

    praticando escrita de variáveis em diferentes data types

--- tmp/teste.php
+++ tmp/teste.php
@@ -2,8 +2,12 @@
     <body>
         <?php 
             echo "hello world, my name is ";
-            $nome = " Ially!";
-            print($nome);
+            $nome = " Ially! ";
+            print "$nome<br>";
+            $boll = true;
+            print "$boll<br>";
+            $group = array("hello", "world");
+            print_r($group);
         ?>
     </body>
 </html>
