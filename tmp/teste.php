8df2a4a0567363ff8c63d1aad54f160e9812678f
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-08-08 12:02:22 -0300

    crinado constantes

--- tmp/teste.php
+++ tmp/teste.php
@@ -13,12 +13,19 @@
 
             class Noticia {
                 function imprimir() {
-                    return "<br><strong>PHP é multifuncional.</strong>";
+                    return "<br><strong>PHP é multifuncional.<br></strong>";
                 }
             }
 
             $noticia = new Noticia;
             print($noticia->imprimir()); /*-> significa a notação de ponto em PHP */
+            
+            define("TOTAL", 100);
+            print(TOTAL . " ");/*1ª forma de escrever constantes*/
+
+            const TOTAL2 = 200;
+            echo  "<br> " . TOTAL2; /*2ª forma de escrever constantes*/
+        
         ?>
     </body>
 </html>
